123a4dea84c8fa1e2f184b8c77fdb5dd0d416ea4
Peter <veionestudio@outlook.com>
2021-02-01 15:38:12 +0800

    first commit

---
+++ medical-appointment-admin/src/main/java/com/think/medical/MedicalAppointmentAdminApplication.java
@@ -0,0 +1,17 @@
+package com.think.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 后台程序入口
+ *
+ * @author veione
+ * @date 2021/02/01
+ */
+@SpringBootApplication
+public class MedicalAppointmentAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalAppointmentAdminApplication.class);
+    }
+}
